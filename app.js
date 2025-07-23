1ed549ec36ac90d4bef45b892f6696f6edfff020
babita patel <babitapatel1005@gmail.com>
2025-07-23 23:14:49 +0530

    add new content

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-// add new feature
-// add button
-//add form
+let btn=document.querySelector("button");
+let ul= document.querySelector("ul");
+let input=document.querySelector("input");
+
+btn.addEventListener("click",function(){
+    let li=document.createElement("li");
+     let dltbtn=document.createElement("button");
+    dltbtn.innerText="Delete";
+    dltbtn.classList.add("delete");
+     li.innerText=input.value;
+    ul.appendChild(li);
+    li.append(dltbtn);
+    input.value="";
+})
+
+let dlt=document.querySelector(".delete");
+
+
+ul.addEventListener("click",function(event){
+    
+ if(event.target.nodeName=="BUTTON"){
+    let listItem=event.target.parentElement;
+    console.log(listItem);
+    listItem.remove();
+  
+ }
+ 
+})
+
 
